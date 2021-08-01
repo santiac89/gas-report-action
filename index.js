a51128df4ef35899e951bbc8053b443781a11877
Santiago Carullo <santiago.carullo@gmail.com>
2021-08-01 13:26:37 +0200

    Use comma separated string for contracts input

--- index.js
+++ index.js
@@ -3,30 +3,30 @@ const github = require('@actions/github');
 const fs = require('fs');
 
 try {
-  // `who-to-greet` input defined in action metadata file
-  const reportFilePath = core.getInput('report_file');
-  const contractsToReport = core.getInput('contracts');
+    const reportFilePath = core.getInput('report_file');
+    let contractsToReport = core.getInput('contracts');
 
-//   
-// console.log(`Hello ${nameToGreet}!`);
-
-
-const rawReport = fs.readFileSync(reportFilePath);
-
-const jsonReport = JSON.parse(rawReport);
-
-const methodsToReport = Object.keys(jsonReport.info.methods).map(key => {
-    if (contractsToReport.length === 0 || contractsToReport.includes(jsonReport.info.methods[key].contract)) {
-        return jsonReport.info.methods[key];
+    if (contractsToReport === '') {
+        contractsToReport = []
+    } else {
+        contractsToReport = contractsToReport.split(',');
     }
-});
+
+    const rawReport = fs.readFileSync(reportFilePath);
+
+    const jsonReport = JSON.parse(rawReport);
+
+    const methodsToReport = Object.keys(jsonReport.info.methods).map(key => {
+        if (contractsToReport.length === 0 || contractsToReport.includes(jsonReport.info.methods[key].contract)) {
+            return jsonReport.info.methods[key];
+        }
+    });
 
 
-//   const time = (new Date()).toTimeString();
-  core.setOutput("github_comment", JSON.stringify(methodsToReport));
+    core.setOutput("github_comment", JSON.stringify(methodsToReport));
 //   // Get the JSON webhook payload for the event that triggered the workflow
 //   const payload = JSON.stringify(github.context.payload, undefined, 2)
 //   console.log(`The event payload: ${payload}`);
 } catch (error) {
-  core.setFailed(error.message);
+    core.setFailed(error.message);
 }
