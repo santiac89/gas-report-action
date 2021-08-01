f070b68389dd4268fa65389e6ab863784fa5ff86
Santiago Carullo <santiago.carullo@gmail.com>
2021-08-01 22:58:11 +0200

    Debug

--- index.js
+++ index.js
@@ -113,31 +113,3 @@ const run = async () => {
 
 run();
 
-
-
-
-
-
-
-
-
-
-console.log(root.firstChild.structure);
-// ul#list
-//   li
-//     #text
-
-console.log(root.querySelector('#list'));
-// { tagName: 'ul',
-//   rawAttrs: 'id="list"',
-//   childNodes:
-//    [ { tagName: 'li',
-//        rawAttrs: '',
-//        childNodes: [Object],
-//        classNames: [] } ],
-//   id: 'list',
-//   classNames: [] }
-console.log(root.toString());
-// <ul id="list"><li>Hello World</li></ul>
-root.set_content('<li>Hello World</li>');
-root.toString();	// <li>Hello World</li>
