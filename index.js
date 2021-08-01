634a2256500105f67486aa56d0810fdc7cd721d8
Santiago Carullo <santiago.carullo@gmail.com>
2021-08-01 16:03:21 +0200

    Comment with results in the PR if present

--- index.js
+++ index.js
@@ -43,14 +43,28 @@ try {
     });
 
     htmlOutput += `</table>`
+    htmlOutput = htmlOutput.replace(/(?:\r\n|\r|\n)/g, '');
 
+    core.setOutput("github_comment", htmlOutput);
 
+    const github_token = core.getInput('github_token');
+
+    const context = github.context;
+    
+    if (context.payload.pull_request == null) {
+        return;
+    }
+
+    const pull_request_number = context.payload.pull_request.number;
+
+    const octokit = new github.GitHub(github_token);
+    const new_comment = octokit.issues.createComment({
+        ...context.repo,
+        issue_number: pull_request_number,
+        body: htmlOutput
+      });
     
 
-    core.setOutput("github_comment", htmlOutput.replace(/(?:\r\n|\r|\n)/g, ''));
-//   // Get the JSON webhook payload for the event that triggered the workflow
-//   const payload = JSON.stringify(github.context.payload, undefined, 2)
-//   console.log(`The event payload: ${payload}`);
 } catch (error) {
     core.setFailed(error.message);
 }
