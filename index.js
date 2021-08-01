8bc0864186013edbba884c12db2c64c9494e5ce5
Santiago Carullo <santiago.carullo@gmail.com>
2021-08-01 16:17:13 +0200

    Debug

--- index.js
+++ index.js
@@ -50,7 +50,7 @@ try {
     const github_token = core.getInput('token');
 
     if (!github_token) return;
-    
+
     const context = github.context;
     
     if (context.payload.pull_request == null) {
@@ -60,6 +60,8 @@ try {
     const pull_request_number = context.payload.pull_request.number;
 
     const octokit = new github.GitHub(github_token);
+
+    console.log('COMMENT?')
     const new_comment = octokit.issues.createComment({
         ...context.repo,
         issue_number: pull_request_number,
